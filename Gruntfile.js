ce51ba0bfad2e9f5d5f4d0146c533f3a498c00dc
bheeshma <bheeshmat@gmail.com>
2016-12-02 20:10:49 +0530

    Heroku uglify using grunt

---
+++ Gruntfile.js
@@ -0,0 +1,17 @@
+module.exports = function(grunt){
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+      },
+      build: {
+        src: 'src/factorial.js',
+        dest: 'build/factorial.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify']);
+}
